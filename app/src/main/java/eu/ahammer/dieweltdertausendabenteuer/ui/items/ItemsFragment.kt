8c6959fda0b16666ab4cce8cc911dc82c292d0a2
Andreas Ahammer <andreas.ahammer@wrwks.at>
2024-11-13 10:47:31 +0100

    remove unused fragments

--- app/src/main/java/eu/ahammer/dieweltdertausendabenteuer/ui/items/ItemsFragment.kt
+++ app/src/main/java/eu/ahammer/dieweltdertausendabenteuer/ui/items/ItemsFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import eu.ahammer.dieweltdertausendabenteuer.databinding.FragmentItemsBinding
-import eu.ahammer.dieweltdertausendabenteuer.databinding.FragmentNotificationsBinding
 
 class ItemsFragment : Fragment() {
 
