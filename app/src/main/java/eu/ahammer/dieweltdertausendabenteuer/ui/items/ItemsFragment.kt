2d05638817d4848e1cefb434a64ef32e7acd151a
Andreas Ahammer <andreas.ahammer@wrwks.at>
2024-11-17 16:31:31 +0100

    add list and add-button for items

--- app/src/main/java/eu/ahammer/dieweltdertausendabenteuer/ui/items/ItemsFragment.kt
+++ app/src/main/java/eu/ahammer/dieweltdertausendabenteuer/ui/items/ItemsFragment.kt
@@ -1,14 +1,26 @@
 package eu.ahammer.dieweltdertausendabenteuer.ui.items
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import eu.ahammer.dieweltdertausendabenteuer.databinding.FragmentItemsBinding
 import eu.ahammer.dieweltdertausendabenteuer.ui.MyFragment
 
 class ItemsFragment : MyFragment<FragmentItemsBinding, ItemsViewModel>(ItemsViewModel::class.java) {
+
+    private lateinit var button: FloatingActionButton
+    private var items: ArrayList<String> = arrayListOf()
+    private lateinit var itemList: ListView
+    private lateinit var itemAdapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,10 +28,42 @@ class ItemsFragment : MyFragment<FragmentItemsBinding, ItemsViewModel>(ItemsView
         savedInstanceState: Bundle?
     ): View {
         withinContext(inflater, container) {
-            val textView: TextView = binding.textItems
-            getViewModel().text.observe(viewLifecycleOwner) {
-                textView.text = it
+
+            button = binding.itemAddButton
+            button.setOnClickListener {
+                val dialog = AlertDialog.Builder(requireContext())
+                dialog.setTitle("Gegenstand hinzufügen")
+
+                val input = EditText(requireContext())
+                input.hint = "Gegenstand eingeben"
+                input.inputType = InputType.TYPE_CLASS_TEXT
+                dialog.setView(input)
+
+                dialog.setPositiveButton("OK") { _, _ ->
+                    items.add(input.text.toString())
+                    Toast.makeText(requireContext(), "Gegenstand hinzugefügt", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                dialog.setNegativeButton("Abbrechen") { _, _ ->
+                    Toast.makeText(requireContext(), "Abgebrochen", Toast.LENGTH_SHORT).show()
+                }
+                dialog.show()
             }
+
+            itemList = binding.itemList
+            itemAdapter =
+                ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, items)
+            itemList.adapter = itemAdapter
+            itemList.onItemLongClickListener =
+                AdapterView.OnItemLongClickListener { _, _, pos, _ ->
+                    val itemText = items[pos]
+                    items.removeAt(pos)
+                    itemAdapter.notifyDataSetChanged()
+                    Toast.makeText(requireContext(), "'$itemText' gelöscht", Toast.LENGTH_SHORT)
+                        .show()
+                    true
+                }
+
             return binding.root
         }
     }
@@ -29,8 +73,10 @@ class ItemsFragment : MyFragment<FragmentItemsBinding, ItemsViewModel>(ItemsView
         destroyContext()
     }
 
-    override fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentItemsBinding {
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentItemsBinding {
         return FragmentItemsBinding.inflate(inflater, container, false)
     }
 }
-
--- app/src/main/java/eu/ahammer/dieweltdertausendabenteuer/ui/items/ItemsViewModel.kt
+++ app/src/main/java/eu/ahammer/dieweltdertausendabenteuer/ui/items/ItemsViewModel.kt
@@ -1,13 +1,7 @@
 package eu.ahammer.dieweltdertausendabenteuer.ui.items
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ItemsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Deine Gegenstände"
-    }
-    val text: LiveData<String> = _text
 }
